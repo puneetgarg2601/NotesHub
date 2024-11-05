91d7c80c351ff0b96c7fb567fc3403bbd21d1ab0
puneetgarg2601 <puneetgarg26012000@gmail.com>
2024-11-05 17:02:58 +0530

    Added models in our project

--- NotesHub/core/admin.py
+++ NotesHub/core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Course, Notes, Bookmark
 
-# Register your models here.
+admin.site.register(Course)
+admin.site.register(Notes)
+admin.site.register(Bookmark)

40c111b648a5aa1614b247c13416b292d3d0ad49
REXRITZ <rmbhadane33@gmail.com>
2024-11-05 18:34:45 +0530

    login feature

--- NotesHub/core/urls.py
+++ NotesHub/core/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),  # Example route
+    path('login', views.loginView, name='login'),
+    path('register', views.registerView, name='register'),
+    path('logout', views.logoutView, name='logout'),
 ]
